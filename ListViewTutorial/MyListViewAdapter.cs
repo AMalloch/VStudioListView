d99a892a22675c020f26b0108d33eaf985e42e31
Angus Malloch <angusbass@hotmail.com>
2018-07-03 00:56:26 +0100

    Override's on getItemId, this, GetView. GetView should add rows of names based on the txtName id. Building at this point with no errors.

--- ListViewTutorial/MyListViewAdapter.cs
+++ ListViewTutorial/MyListViewAdapter.cs
@@ -26,5 +26,30 @@ namespace ListViewTutorial
         {
             get { return mItems.Count; }
         }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override string this[int position]
+        {
+            get { return mItems[position]; }
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View row = convertView;
+
+            if( row == null)
+            {
+                row = LayoutInflater.From(mContext).Inflate(Resource.Layout.listview_row, null, false);
+            }
+             
+            TextView txtName = row.FindViewById<TextView>(Resource.Id.txtName);
+            txtName.Text = mItems[position];
+
+            return row;
+        } 
     }
-}
+} 
