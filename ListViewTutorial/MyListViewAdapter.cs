a254f20c0b21ca94282c7ae12f2d6a1b882ada5f
Angus Malloch <angusbass@hotmail.com>
2018-07-03 00:42:40 +0100

    Created Adapter class with global variables, constructor and override count method.

---
+++ ListViewTutorial/MyListViewAdapter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ListViewTutorial
+{
+    class MyListViewAdapter : BaseAdapter<string>
+    {
+        private List<string> mItems;
+        private Context mContext;
+
+        public MyListViewAdapter(Context context, List<string> items)
+        {
+            mItems = items;
+            mContext = context;
+        }
+        public override int Count
+        {
+            get { return mItems.Count; }
+        }
+    }
+}
