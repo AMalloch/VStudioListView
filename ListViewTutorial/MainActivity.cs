eaa79988578bc2af69281a995a1c542786785f10
Angus Malloch <angusbass@hotmail.com>
2018-07-02 20:26:16 +0100

    Add project files.

---
+++ ListViewTutorial/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System.Collections.Generic;
+
+namespace ListViewTutorial
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        private List<string> mItems;
+        private ListView mListView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            mListView = FindViewById<ListView>(Resource.Id.myListView);
+
+            mItems = new List<string>();
+
+            mItems.Add("Angus");  
+            mItems.Add("Heather");
+            mItems.Add("Bonzo");
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mItems);
+
+            mListView.Adapter = adapter;
+        }
+    }
+}
+
